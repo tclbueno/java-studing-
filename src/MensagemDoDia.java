ad99a91614e3e9fd9d5d3395af0c0fafb84b1463
Tainara Bueno <107087909+tclbueno@users.noreply.github.com>
2025-07-10 16:11:41 -0300

    Add MensagemDoDia.java
    Studing IF

---
+++ src/MensagemDoDia.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+public class MensagemDoDia{
+    public static void main(String[] args) {
+       int numero;
+       
+       Scanner scanner = new Scanner(System.in);
+
+       System.out.println("Digite um núemro de 1 a 10:");
+         numero = scanner.nextInt();
+
+         if(numero >= 1 && numero <=5){
+            System.out.println("Se não arriscarmos, não criamos o futuro! -Luffy (One Piece)");
+            } if (numero >= 6 && numero <= 10){
+                System.out.println("Levante a cabeça, prossiga com sua vida. Você tem suas próprias pernas para poder andar, então use-as! -Edward Elric (Fullmetal Alchemist)");       
+         }
+        
+    }
+
+}
