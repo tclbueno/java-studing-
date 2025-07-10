03b0d6346ee5d5c9e33e94f7437daf06decad703
tclbueno <tainara.lucindobueno@hotmail.com>
2025-07-09 22:15:23 -0300

    estudando charAt, lenght e equals

--- src/FichaCadastral.java
+++ src/FichaCadastral.java
@@ -1,3 +1,7 @@
+
+
+
+
 public class FichaCadastral {
     public static void main(String[] args) throws Exception {
         String primeiroNome = "Tainara";
@@ -14,5 +18,11 @@ public class FichaCadastral {
         System.out.println("Data de Nascimento: " + dataNascimento);
         System.out.println("Altura: " + altura + " m");
         System.out.println("Possui CNH: " + (possuiCNH ? "Sim" : "Não"));
+        System.out.println("A primeira letra de seu nome é: " + primeiroNome.charAt(0)); // Imprimindo o primeiro caractere do primeiro nome
+        System.out.println("A ultima letra de seu nome é: " + primeiroNome.charAt(primeiroNome.length()-1)); // Imprimindo o último caractere do primeiro nome
+        System.out.println("Seu primeitro nome tem " + primeiroNome.length() + " letras"); // Imprimindo o tamanho do primeiro nome
+        System.out.println("Seu ultimo nome tem " + ultimoNome.length() + " letras"); // Imprimindo o tamanho do último nome
+        System.out.println("Seu nome completo tem " + (primeiroNome.length() + ultimoNome.length()) + " letras"); // Imprimindo o tamanho do nome completo
+        System.out.println(primeiroNome.equals("Tatiana")); // Verificando se o primeiro nome é igual a "Tatiana"
     }
 }
